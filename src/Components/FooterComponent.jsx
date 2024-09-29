1601ae70bcd4886e7ce72eb221b8be0e186dce2c
Nikhil Pathak <nikhilpathak2602@gmail.com>
2024-09-30 00:50:15 +0530

    footer designing and styling

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -1,9 +1,39 @@
-const FooterComponent=()=>{
-    return (
+import { Link } from "react-router-dom";
+
+const FooterComponent = () => {
+  return (
+    <div id="footer-container">
+      <div className="footer-image-container">
+        <img
+          src="./footer-logo3.png"
+          alt="footer-logo-image"
+          className="footer-logo-img"
+        />
+      </div>
+      <div className="footer-links-container">
+        <h1>Links</h1>
         <div>
-            Footer Component
+          <Link to="/" className="footer-link">
+            Home
+          </Link>
+          <Link to="/browse-books" className="footer-link">
+            Browse Books
+          </Link>
+          <Link to="/add-new-book" className="footer-link">
+            Add New Books
+          </Link>
         </div>
-    )
-}
+      </div>
+      <div className="footer-form-container">
+        <p style={{fontWeight:500}}>Subscribe For Our Newletter</p>
+        <div>
+          {" "}
+          <input type="text" placeholder="Enter Your Email Address" className="footer-input" required/>
+          <button className="footer-form-btn">Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default FooterComponent;
